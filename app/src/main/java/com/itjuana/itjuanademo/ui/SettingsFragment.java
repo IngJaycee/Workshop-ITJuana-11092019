ac6444059f586539f08541b84971d7dbb15c1491
Juan Carlos Torres <jutorres@insulet.com>
2019-11-08 19:47:27 -0800

    point1.1

---
+++ app/src/main/java/com/itjuana/itjuanademo/ui/SettingsFragment.java
@@ -0,0 +1,28 @@
+package com.itjuana.itjuanademo.ui;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.itjuana.itjuanademo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
+    }
+
+}
